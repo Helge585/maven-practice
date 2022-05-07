a5ff11322d7aae4f85b936aa9290c5b06ba832e4
Oleg Kuznetsov <oleg.kyznetzov@mail.ru>
2022-05-07 15:21:05 +0400

    javadoc files

---
+++ target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.kuznetsov.oleg","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"ru.kuznetsov.oleg","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"ru.kuznetsov.oleg","l":"App"}];updateSearchResults();
